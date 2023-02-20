caed644dc9921465217e3784d3b89923751788b2
Moch Khrisna Arsita [MCG] <moch.arsita@merdekacoppergold.com>
2023-02-20 09:59:06 +0700

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "merdekatest-39025",
   url: "https://merdekatest-39025.botics.co",
   api: "https://merdekatest-39025.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
